a66b5eee4a201c518548a8c025de1ea490da4276
Daniel Sasse <dsasse07@gmail.com>
2020-11-23 04:27:58 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -5,7 +5,7 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-  binding.pry
+  
   cart = consolidate_cart(cart)
   cart.each do |cart_item|
     coupons.each do |coupon_item|
